d830baca88b9d5185073687b375872f902e1bf71
Anisha patel <162287095+AnishaPatel14@users.noreply.github.com>
2024-10-02 16:50:51 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,6 +1,7 @@
 package JavaSwing;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,22 +13,48 @@ import javax.swing.JTextField;
 
 public class LoginPage extends JFrame implements ActionListener{
 	
-	JLabel l1,l2;
+	JLabel lbl,l1,l2;
 	JTextField t1;
 	JPasswordField t2;
 	JButton b, close;
 	
 	LoginPage(){
 		setTitle("Login Page");
-		setLayout(new FlowLayout());
+		setLayout(null);
 		setSize(600,600);
-		l1 = new JLabel("Username");
-		l2 = new JLabel("Password");
-		t1 = new JTextField(20);
-		t2 = new JPasswordField(10);
-		b = new JButton("Login");
-		close = new JButton("Close");
 		
+		lbl = new JLabel("Login Form");
+		lbl.setSize(200,40);
+		lbl.setLocation(250,20);
+		Font f = new Font("arial", Font.BOLD, 19);
+		lbl.setFont(f);
+		
+		l1 = new JLabel("Username");
+		l1.setSize(100,30);
+		l1.setLocation(200,90);
+		
+		l2 = new JLabel("Password");
+		l2.setSize(100,30);
+		l2.setLocation(200,150);
+		
+		t1 = new JTextField(20);
+		t1.setSize(200,30);
+		t1.setLocation(300,90);
+		
+		t2 = new JPasswordField(20);
+		t2.setSize(200,30);
+		t2.setLocation(300,150);
+		
+		b = new JButton("Login");
+		b.setSize(100,30);
+		b.setLocation(200,230);
+		
+		close = new JButton("Close");
+		close.setSize(100,30);
+		close.setLocation(350,230);
+		
+		
+		add(lbl);
 		add(l1);
 		add(t1);
 		add(l2);
@@ -56,6 +83,5 @@ public class LoginPage extends JFrame implements ActionListener{
 		l.setLocation(300,300);
 	}
 
-	
-
 }
+
