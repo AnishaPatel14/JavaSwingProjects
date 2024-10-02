de88c339b7ae6027a65793d49eb92130103fef09
Anisha patel <162287095+AnishaPatel14@users.noreply.github.com>
2024-10-02 16:35:14 +0530

    Update SimpleIntrestCalculator.java

--- SimpleIntrestCalculator.java
+++ SimpleIntrestCalculator.java
@@ -3,19 +3,24 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleIntrestCalculator extends Frame{
+public class SICalcActive extends Frame implements ActionListener{
 	
 	Label l1,l2,l3,l4;
 	TextField t1,t2,t3,t4;
 	Button b1,b2,b3;
 	
-	public SimpleIntrestCalculator(){
+	public SICalcActive(){
 		
-		l1 = new Label("Priciple Amount :     ");
-		l2 = new Label("Rate :     ");
-		l3 = new Label("Time Duration :     ");
-		l4 = new Label("Simple Intrest :     ");
+		setSize(250,500);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		l1 = new Label("Principle");
+		l2 = new Label("Rate");
+		l3 = new Label("Time");
+		l4 = new Label("Simple Intrest");
 		
 		t1 = new TextField(20);
 		t2 = new TextField(20);
@@ -25,7 +30,6 @@ public class SimpleIntrestCalculator extends Frame{
 		b1 = new Button("Calculate");
 		b2 = new Button("Reset");
 		b3 = new Button("Close");
-		
 		
 		add(l1);
 		add(t1);
@@ -39,19 +43,56 @@ public class SimpleIntrestCalculator extends Frame{
 		add(b2);
 		add(b3);
 		
-		setSize(700,400);
-		setTitle("SI Calculator");
-
-		setLayout(new FlowLayout()); // align center
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
 		
 	}
 	
-
-	public static void main(String[] args) {
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		double p = 0, r = 0, t = 0, si = 0;
 		
-		SimpleIntrestCalculator s = new SimpleIntrestCalculator();
-		s.setVisible(true);
+		try {
+			p = Double.parseDouble(t1.getText());
+		} catch (NumberFormatException e) {
+			t1.setText("Only numbers are allowed");
+		}
+		
+		try {
+			r = Double.parseDouble(t2.getText());
+		} catch (NumberFormatException e) {
+			t2.setText("Only numbers are allowed");
+		}
+		
+		try {
+			t = Double.parseDouble(t3.getText());
+		} catch (NumberFormatException e) {
+			t3.setText("Only numbers are allowed");
+		}
+		
+		if(ae.getSource() == b1) {
+			si = (p*r*t)/100;
+			t4.setText(String.valueOf(si));
+		}
+		if(ae.getSource() == b2) {
+			t1.setText("");
+			t2.setText("");
+			t3.setText("");
+			t4.setText("");
+		}
+		if(ae.getSource() == b3) {
+			System.exit(0);
+		}
 		
 	}
 
+	public static void main(String[] args) {
+		SICalcActive si = new SICalcActive();
+		si.setVisible(true);
+		si.setLocation(400,400);
+
+	}	
+
 }
+
